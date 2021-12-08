ddfbdfcbc628b121eac0f9e8ce9072ad4342fab9
sahana178 <85974914+sahana178@users.noreply.github.com>
2021-12-08 17:48:52 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,29 @@
+function adduser(){
+    username=document.getElementById("user_name").value;
+    localStorage.setItem("user_name", username);
+    window.location="kwitter_room.html";
+}
+
+
+function addroom(){
+    room_name = document.getElementById("add_room").value;
+    firebase.database().ref("/").child(room_name).update({
+          purpose : "adding room name"
+    });
+}
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+Room_names = childKey;
+//Start code
+console.log("Room Name - " + Room_names );
+row = "<div class = 'room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div> <hr>";
+document.getElementById("output").innerHTML += row;
+//End code
+});});}
+getData();
+
+function  redirectToRoomName(name){
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "kwitter.html";
+ 
+ }
